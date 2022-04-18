6d07a2d864f126d8e8395d668dea76c782b2af2e
oitowl7 <shear_j@hotmail.com>
2022-04-17 22:29:22 -0400

    updated storybook for footer

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -6,14 +6,6 @@ import { Footer } from './Footer';
 const meta: Meta = {
   component: Footer,
   title: 'Components/Footer',
-  argTypes: {
-    url: {
-      description: 'The external url to use for the Footer. Will override the default if passed.'
-    },
-    maxHeight: {
-      description: 'Number value of the max height of the image in pixels. Defaults to 20.'
-    }
-  }
 }
 
 export default meta;
@@ -23,10 +15,4 @@ const Template: Story = args => <Footer {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   maxHeight: 20,
-};
-
-export const Override =  Template.bind({});
-Override.args = {
-  ...Default.args,
-  url: 'https://www.singlestoneconsulting.com/wp-content/uploads/2019/10/logo_primary-1.svg'
 };
